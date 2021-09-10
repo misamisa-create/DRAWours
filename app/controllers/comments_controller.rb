c90bca0eacc5125aa7d793fae53cd3cd6bbb583e
misamisa-create <missmis0329@gmail.com>
2021-09-10 05:21:05 +0000

    [fix]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_path(params[:post_id])
   end
 
   private
