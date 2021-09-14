0545de4184bbb760b5aa5631d94906b40bc219d3
misamisa-create <missmis0329@gmail.com>
2021-09-14 05:22:18 +0000

    [add]

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,8 @@ class Chat < ApplicationRecord
   belongs_to :room
   belongs_to :user
   # うまくかけられない
-  # validates :message, {presence: true}
+  # validates :message, presence: true
+  # validates :message, presence: true, on: :save
 
 
 end
