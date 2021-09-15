37ab9a6deffb4e04ba5340c9c65a4501b4feef13
misamisa-create <missmis0329@gmail.com>
2021-09-15 01:56:56 +0000

    [add]チャット非同期途中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,15 +6,14 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
     @comment.save
-    # redirect_to post_path(post)
+
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.find(params[:id])
     @comment.destroy
-    # Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    # redirect_to post_path(params[:post_id])
+
   end
 
   private
