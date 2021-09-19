c70fead77cc715df0d32a8ad028d54711302899c
misamisa-create <missmis0329@gmail.com>
2021-09-19 03:37:49 +0000

    [add]トップのアニメーション

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,20 @@
 //= require turbolinks
 //= require_tree .
 
+// トップページアニメーション
+$(function(){
+  setTimeout(function(){
+    $('.start p').fadeIn(1600);
+  },500);
+  setTimeout(function(){
+    $('.start').fadeOut(500);
+  },2500);
+})
+
 $(function(){
   // 初期画像の表示
   let index = 0;
-  
+
   $('.img').eq(index).addClass('current-img');
   // 一定時間ごとに特定の処理を繰り返す
   setInterval(function(){
