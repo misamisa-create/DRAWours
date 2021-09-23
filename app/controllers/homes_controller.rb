a9e76fe6679c869bf45169321114044dcaef10a0
misamisa-create <missmis0329@gmail.com>
2021-09-23 05:59:14 +0000

    [add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
   def top
     @posts = Post.all
+    # 投稿数が多くなってきたらこれに変えたい
+    # @posts = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) desc').limit(10).pluck(:post_id))
   end
 end
