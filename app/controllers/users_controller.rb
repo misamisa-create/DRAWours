165a720ca95c06d359615b5bbe6f02c21735fce8
misamisa-create <missmis0329@gmail.com>
2021-09-23 04:07:30 +0000

    [add]レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     # あとでfavoriteなども追加していく
     @posts_all = Post.includes(:user)
     @user = User.find(params[:id])
-    @posts = @posts_all.where(user_id: @user).order("created_at DESC")
+    @posts = @posts_all.where(user_id: @user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def update
