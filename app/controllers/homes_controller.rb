4df0e7a1af4bd794d43f6ef1d517bef7662c6431
misamisa-create <missmis0329@gmail.com>
2021-09-27 01:08:36 +0000

    [add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
     # @posts = Post.limit(5)
-    @posts = Post.limit(5).with_attached_image
+    @posts = Post.includes(:user).limit(5).with_attached_image
   end
 end
